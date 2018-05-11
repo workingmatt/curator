bcc1a30512e0516500407c2ab14274bca9405453
Matt Mapleston <matt@chanzachanzo.com>
2018-05-11 17:04:45 +0100

    Bug fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 app.use((req, res, next) => {
 	next()
 })
-app.use(express.static(__dirname+'./public/'));
+app.use(express.static(__dirname+'/public/'));
 
 //routes
 app.get('/', (req,res) => {
